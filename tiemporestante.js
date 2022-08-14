7366d43865f6ad143bc0f5623cb8e0133b398697
shencanet <santi.dionis@gmail.com>
2022-08-14 18:49:23 +0200

    desafio 3 calcular fechas resuleto

--- tiemporestante.js
+++ tiemporestante.js
@@ -1,3 +1,31 @@
-const fechaFinal = new Date('2050-06-27');
+const fechaFinal = new Date('1976-04-');
 const resultado = document.getElementById('resultado');
 
+const ms_anno = 1000 * 60 *60 * 24 * 365;
+const ms_mes = 1000 * 60 *60 * 24 * 30;
+const ms_dia = 1000 * 60 *60 * 24;
+const ms_hora = 1000 * 60 *60;
+const ms_minuto = 1000 * 60;
+const ms_segundo = 1000;
+
+
+const hoy = new Date();
+
+let resto;
+
+const diffMilisegundos =  Math.abs(fechaFinal - hoy);
+
+const anno = Math.floor(diffMilisegundos /ms_anno);
+const mes = Math.floor(diffMilisegundos /ms_mes);
+const dia = Math.floor(diffMilisegundos /ms_dia);
+const hora = Math.floor(diffMilisegundos /ms_hora);
+const minuto = Math.floor(diffMilisegundos /ms_minuto);
+const segundo = Math.floor(diffMilisegundos /ms_segundo);
+
+
+console.log(anno);
+console.log(mes);
+console.log(dia);
+console.log(hora);
+console.log(minuto);
+console.log(segundo);
